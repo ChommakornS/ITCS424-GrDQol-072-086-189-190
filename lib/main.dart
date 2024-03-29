817de46ac1c0f184598f968fe46238d7ece5f266
pachanitha <142656246+pachanitha@users.noreply.github.com>
2024-03-29 23:15:48 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:news_app/Config/Theme.dart';
 import 'package:news_app/Demopage.dart';
 import 'package:news_app/HomePageController.dart';
+import 'package:news_app/Pages/OnBoardingPage/OnBoard.dart';
+import 'package:news_app/Pages/screens/signup_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +27,7 @@ class MyApp extends StatelessWidget {
       theme: lightTheme,
       themeMode: ThemeMode.system,
       darkTheme: darkTheme,
-      home: const HomePageController(),
+      home: const OnBoardPage(),
     );
   }
 }
